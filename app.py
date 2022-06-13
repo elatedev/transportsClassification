edcbdc3d2111b41000c72b442f859e6006ec340b
elatedev <elatedev@gmail.com>
2022-06-13 21:12:31 +0500

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st 
+from fastai.vision.all import * 
+import pathlib
+import plotly.express as px
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+st.title("Transportni klassifikatsiya qiluvchi modelll")
+
+# uploading an image
+
+file = st.file_uploader('Rasm Yuklash', type=['png', 'jpeg', 'gif', 'svg', 'jpg'])
+
+if file:
+    # model
+    model = load_learner('transport_model.pkl')
+    st.image(file)
+    img = PILImage.create(file)
+    # prediction
+    pred, pred_id, probs = model.predict(img)
+
+    st.success(f"Prediction: {pred}")
+    st.info(f"Accuracy: {probs[pred_id]*100:.1f}%")
+
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
